461d23e11ffedab40e6feb5a1a5709d391f0e251
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-15 14:20:51 -0500

    Create index and booking_params action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starting_date, :end_date, :status)
+  end
 end
