e13cc667dfd8c9ebae0d225a9fbcf0d9a7be5cdf
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-17 22:42:40 -0500

    Define enum in booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,10 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :starting_date, :end_date, presence: true
+
+  enum status: {
+    pending: 0,
+    accepted: 1,
+    declined: 2
+  }
 end
