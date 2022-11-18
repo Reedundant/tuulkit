344788fb285010e58af34d9184f6e8fef9010803
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-17 23:55:12 -0500

    Implement status update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,11 +2,12 @@ class Booking < ApplicationRecord
   belongs_to :tool
   belongs_to :user
 
-  validates :starting_date, :end_date, presence: true
-
   enum status: {
     pending: 0,
     accepted: 1,
     declined: 2
   }
+
+  validates :starting_date, :end_date, presence: true
+
 end
